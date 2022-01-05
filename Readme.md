74cec79d56feff6d72d5833f12bd8543ba4e0368
Ray <qc@pop-os.localdomain>
2022-01-05 21:05:37 +0530

    readme added

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # Web Speech API with Voice Commands
 
-# Live-Demo: 
+# Live-Demo: https://raydcode.github.io/web_speech.withcommands/
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import * as ele from './utils/elements.js';
-import * as fun from './utils/functions.js';
 
 const speechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -21,7 +20,8 @@ if (speechRecognition) {
   recognition.addEventListener('start', () => {
     ele.mic.classList.remove('fa-microphone');
     ele.mic.classList.add('fa-microphone-slash');
-    ele.instruction.textContent = 'Listening....';
+    // stop use hotkey
+    ele.instruction.textContent = 'Listening.... Press Ctrl + m to stop';
     ele.searchInput.focus();
     console.log('Speech is activated !!!');
   });
@@ -29,22 +29,42 @@ if (speechRecognition) {
   recognition.addEventListener('end', () => {
     ele.mic.classList.remove('fa-microphone-slash');
     ele.mic.classList.add('fa-microphone');
-    ele.instruction.textContent = 'Click the Mic icon to start';
+    // start use hotkey
+    ele.instruction.textContent =
+      'Press Ctrl + x or Click the Mic icon to start';
     ele.searchInput.focus();
     console.log('Speech is Deactivated!!!');
   });
 
   recognition.continuous = true;
 
-  let content = '';
-
   recognition.addEventListener('result', (e) => {
     console.log(e);
     const speech = e.resultIndex;
     const transcript = e.results[speech][0].transcript;
-    content += transcript;
-    ele.searchInput.value = content;
-    searchInput.focus();
+
+    if (transcript.toLowerCase().trim() == 'stop recording') {
+      recognition.stop();
+    } else if (!ele.searchInput.value) {
+      ele.searchInput.value = transcript;
+    } else {
+      if (transcript.toLowerCase().trim() == 'search') {
+        ele.searchForm.submit();
+      } else if (transcript.toLowerCase().trim() == 'reset') {
+        ele.searchInput.value = '';
+      } else {
+        ele.searchInput.value = transcript;
+      }
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key === 'x') {
+      recognition.start();
+    }
+    if (e.ctrlKey && e.key === 'm') {
+      recognition.stop();
+    }
   });
 } else {
   alert('Speech Web api is not supported');
